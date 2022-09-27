a4f094758249418477b0d67cbce070b13dab6150
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-27 16:02:13 +0200

    Restructuring folders in DCI_ASSESSMENTS

---
+++ python-basics-functions-parameters-i-Capt-V570/Functions_parameters.py
@@ -0,0 +1,64 @@
+##############################################
+###### FUNCTIONS PARAMETERS - EXERCISES ######
+##############################################
+
+# Task 1
+
+print("\n>>> Task 1 <<<\n")
+
+num = [0, 1, 657842]
+
+'''
+This function divides number by modulus 2 and return Bool value True for odd numbers and False for even numbers
+'''
+def isOdd(num):        
+    if num % 2 != 0:
+        return True
+    else:
+        return False
+
+
+'''
+This function divides number by modulus 2 and return Bool value True for even number and False for odd numbers
+'''
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print(isOdd(1), isEven(1))
+print(isOdd(657842), isEven(657842))
+print(isOdd(0), isEven(0))
+
+# Task 2
+
+print("\n>>> Task 2 <<<\n")
+
+number = (input("Enter a number here: "))
+
+def getParity():
+    if int(number) % 2 == 0:
+        print("Even")
+        return True
+    else:
+        print("Parity indicated is unknown")
+        return False
+
+print(getParity(-2, 'number'))
+
+
+
+'''
+# Task 3
+
+print("\n>>> Task 3 <<<\n")
+
+# Task 4
+
+print("\n>>> Task 4 <<<\n")
+
+# Task 5
+
+print("\n>>> Task 5 <<<\n")
+'''
