651e4dd7295d9da86cea54f8a5dda589d0683feb
Fausto Ferrara <faustoferrara@gmail.com>
2022-09-27 15:08:39 +0200

    Task 1 done partially wrong

---
+++ python-basics-functions-parameters-i-Capt-V570/Functions_parameters.py
@@ -0,0 +1,56 @@
+##############################################
+###### FUNCTIONS PARAMETERS - EXERCISES ######
+##############################################
+
+# Task 1
+
+print("\n>>> Task 1 <<<\n")
+
+num = int
+
+'''
+This function divides number by modulus 2 and return Bool value True for odd numbers and False for even numbers
+'''
+def isOdd(num):        
+    if num % 2 != 0:
+        print(True)
+    else:
+        return False
+
+
+'''
+This function divides number by modulus 2 and return Bool value True for even number and False for odd numbers
+'''
+def isEven(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        return False
+
+print(isOdd(1), isEven(1))
+print(isOdd(657842), isEven(657842))
+print(isOdd(0), isEven(0))
+
+
+
+
+'''
+
+
+# Task 2
+
+print("\n>>> Task 2 <<<\n")
+
+# Task 3
+
+print("\n>>> Task 3 <<<\n")
+
+# Task 4
+
+print("\n>>> Task 4 <<<\n")
+
+# Task 5
+
+print("\n>>> Task 5 <<<\n")
+
+'''
